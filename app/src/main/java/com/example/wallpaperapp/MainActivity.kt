b975690ba9a7c5b843d5a0691afe34b0c14db7a2
coder <soumyodipsutra@gmail.com>
2023-09-28 21:04:06 +0530

    fragment replacement function has been created

--- app/src/main/java/com/example/wallpaperapp/MainActivity.kt
+++ app/src/main/java/com/example/wallpaperapp/MainActivity.kt
@@ -4,14 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import androidx.fragment.app.Fragment
+import com.example.wallpaperapp.Fragments.DownloadFragment
+import com.example.wallpaperapp.Fragments.HomeFragment
+import com.example.wallpaperapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.icHome.setOnClickListener{
+            replaceFragment(HomeFragment())
+        }
+
+        binding.icDownload.setOnClickListener{
+            replaceFragment(DownloadFragment())
+        }
+    }
+    fun replaceFragment(fragment:Fragment){
+        supportFragmentManager.beginTransaction().add(R.id.fragmentReplace,fragment).commit()
     }
 }
