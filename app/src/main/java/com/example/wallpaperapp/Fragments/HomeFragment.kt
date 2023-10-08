7f5462bc34cb4e95c0216d678fecb084e2b33533
coder <soumyodipsutra@gmail.com>
2023-10-08 23:00:51 +0530

    creating adapter of best of month to fetch data

--- app/src/main/java/com/example/wallpaperapp/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/wallpaperapp/Fragments/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wallpaperapp.Model.BomModel
 import com.example.wallpaperapp.R
 import com.example.wallpaperapp.databinding.FragmentHomeBinding
@@ -36,6 +37,9 @@ class HomeFragment : Fragment() {
             }
 
         }
+
+        binding.rcvBom.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, true)
+        binding.rcvBom.adapter
 
         return binding.root
     }
---
+++ app/src/main/java/com/example/wallpaperapp/adapter/BomAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.wallpaperapp.adapter
+
+class BomAdapter {
+}
