6f63cbc2a2171ba60690062a22b9df61f38a5aa2
coder <soumyodipsutra@gmail.com>
2023-10-09 22:27:18 +0530

    creating the adapter class for best of month

--- app/src/main/java/com/example/wallpaperapp/adapter/BomAdapter.kt
+++ app/src/main/java/com/example/wallpaperapp/adapter/BomAdapter.kt
@@ -1,4 +1,22 @@
 package com.example.wallpaperapp.adapter
 
-class BomAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class BomAdapter : RecyclerView.Adapter<BomAdapter.bomViewHolder>() {
+
+    inner class bomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): bomViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: bomViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
