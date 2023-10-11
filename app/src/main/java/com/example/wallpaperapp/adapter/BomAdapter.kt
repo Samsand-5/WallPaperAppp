73512caaa4904103d51139a272a1dc2fb00cc6b5
coder <soumyodipsutra@gmail.com>
2023-10-11 22:33:43 +0530

    adapter coded with no errors

--- app/src/main/java/com/example/wallpaperapp/adapter/BomAdapter.kt
+++ app/src/main/java/com/example/wallpaperapp/adapter/BomAdapter.kt
@@ -4,13 +4,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.wallpaperapp.Model.BomModel
 import com.example.wallpaperapp.R
 
 class BomAdapter(val requireContext: Context, val listBestOfTheMonth: ArrayList<BomModel>) : RecyclerView.Adapter<BomAdapter.bomViewHolder>() {
 
-    inner class bomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class bomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val imageView = itemView.findViewById<ImageView>(R.id.bom_image)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): bomViewHolder {
         return bomViewHolder(LayoutInflater.from(requireContext).inflate(R.layout.item_bom,parent,false))
@@ -21,6 +25,6 @@ class BomAdapter(val requireContext: Context, val listBestOfTheMonth: ArrayList<
     }
 
     override fun onBindViewHolder(holder: bomViewHolder, position: Int) {
-
+        Glide.with(requireContext).load(listBestOfTheMonth[position].link).into(holder.imageView)
     }
 }
