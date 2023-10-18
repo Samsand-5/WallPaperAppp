87727c98215fdfdd2d017d85aa7dbcc1217d49e6
coder <soumyodipsutra@gmail.com>
2023-10-18 07:49:09 +0530

    some error eliminated

--- app/src/main/java/com/example/wallpaperapp/adapter/ColorToneAdapter.kt
+++ app/src/main/java/com/example/wallpaperapp/adapter/ColorToneAdapter.kt
@@ -5,10 +5,8 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.wallpaperapp.Model.ColorToneModel
 import com.example.wallpaperapp.R
 
