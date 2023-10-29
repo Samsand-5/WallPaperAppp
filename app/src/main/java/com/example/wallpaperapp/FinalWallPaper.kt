7e4c35890d8f59015b021c14c7050f1e9cb4ca40
coder <soumyodipsutra@gmail.com>
2023-10-29 18:16:15 +0530

    MAKING WALLPAPER DOWNLOAD SCREEN

---
+++ app/src/main/java/com/example/wallpaperapp/FinalWallPaper.kt
@@ -0,0 +1,11 @@
+package com.example.wallpaperapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinalWallPaper : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_final_wall_paper)
+    }
+}
