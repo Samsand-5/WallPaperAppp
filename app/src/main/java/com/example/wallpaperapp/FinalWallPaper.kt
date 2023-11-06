49205c66587ed5fa538e8d663c3608ac04c2dcf3
coder <soumyodipsutra@gmail.com>
2023-11-06 12:52:30 +0530

    using coroutines to download and making saveImage function

--- app/src/main/java/com/example/wallpaperapp/FinalWallPaper.kt
+++ app/src/main/java/com/example/wallpaperapp/FinalWallPaper.kt
@@ -1,10 +1,18 @@
 package com.example.wallpaperapp
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.example.wallpaperapp.databinding.ActivityFinalWallPaperBinding
-import com.example.wallpaperapp.databinding.ActivityMainBinding
+import com.google.firebase.inject.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.net.URL
 
 class FinalWallPaper : AppCompatActivity() {
 
@@ -16,15 +24,35 @@ class FinalWallPaper : AppCompatActivity() {
 
         val url = intent.getStringExtra("link")
 
+        val urlImage = URL(url)
+
         //setting wallpaper in @+id/finalWallPaper
         Glide.with(this).load(url).into(binding.finalWallPaper)
 
         binding.btnDownload.setOnClickListener {
+            val result: kotlinx.coroutines.Deferred<Bitmap?> = GlobalScope.async {
+                urlImage.toBitmap(url)
+            }
 
+            GlobalScope.launch(Dispatchers.Main) {
+                saveImage(result.await())
+            }
         }
 
         binding.btnSetWallpaper.setOnClickListener {
 
+        }
+    }
+
+    private fun saveImage(await: Bitmap?) {
+
+    }
+
+    fun URL.toBitmap(url: String?): Bitmap? {
+        return try {
+            BitmapFactory.decodeStream(openStream())
+        } catch (e: IOException) {
+            null
         }
     }
 }
