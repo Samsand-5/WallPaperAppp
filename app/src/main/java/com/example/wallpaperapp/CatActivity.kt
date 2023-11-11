69a80ea1549843825e7af2d9835abbb81a60150b
coder <soumyodipsutra@gmail.com>
2023-11-11 18:43:02 +0530

    new activity created

--- app/src/main/java/com/example/wallpaperapp/CatActivity.kt
+++ app/src/main/java/com/example/wallpaperapp/CatActivity.kt
@@ -2,10 +2,14 @@ package com.example.wallpaperapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wallpaperapp.databinding.ActivityCatBinding
 
 class CatActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityCatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cat)
+        binding = ActivityCatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
