796816f405d0a288cba6e3500c52952913e0df42
coder <soumyodipsutra@gmail.com>
2023-11-13 16:48:06 +0530

    working on backend part

--- app/src/main/java/com/example/wallpaperapp/CatActivity.kt
+++ app/src/main/java/com/example/wallpaperapp/CatActivity.kt
@@ -1,15 +1,34 @@
 package com.example.wallpaperapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.wallpaperapp.Model.CatModel
+import com.example.wallpaperapp.adapter.CatAdapter
 import com.example.wallpaperapp.databinding.ActivityCatBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class CatActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityCatBinding
+    lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = FirebaseFirestore.getInstance()
+        val uid = Intent.getStringExtra("uid")
+
+        db.collection("categories").document(uid!!).addSnapshotListener { value, error ->
+            val listCategories = arrayListOf<CatModel>()
+            val data = value?.toObjects(CatModel::class.java)
+            listCategories.addAll(data!!)
+
+
+        }
+
+
     }
 }
