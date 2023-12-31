1e429633af681dcebd552ae0e5e7e96db5e13fb5
coder <soumyodipsutra@gmail.com>
2023-12-31 09:55:21 +0530

    getting path directory

--- app/src/main/java/com/example/wallpaperapp/Fragments/DownloadFragment.kt
+++ app/src/main/java/com/example/wallpaperapp/Fragments/DownloadFragment.kt
@@ -1,12 +1,14 @@
 package com.example.wallpaperapp.Fragments
 
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.wallpaperapp.R
 import com.example.wallpaperapp.databinding.FragmentDownloadBinding
+import java.io.File
 
 
 class DownloadFragment : Fragment() {
@@ -18,6 +20,13 @@ class DownloadFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentDownloadBinding.inflate(layoutInflater,container,false)
+
+        val allFiles:Array<File>
+        val imageList= arrayListOf<String>()
+
+        val target=Environment.getExternalStorageDirectory().absolutePath+"/Pi"
+
+
         return binding.root
     }
 
