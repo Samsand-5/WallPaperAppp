0debe950f0aeadd68ed29c3f348a2a28aef316fe
coder <soumyodipsutra@gmail.com>
2023-12-31 09:53:21 +0530

    making fragment download binding

--- app/src/main/java/com/example/wallpaperapp/Fragments/DownloadFragment.kt
+++ app/src/main/java/com/example/wallpaperapp/Fragments/DownloadFragment.kt
@@ -6,17 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.wallpaperapp.R
+import com.example.wallpaperapp.databinding.FragmentDownloadBinding
 
 
 class DownloadFragment : Fragment() {
 
-
+    lateinit var binding: FragmentDownloadBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_download, container, false)
+        binding = FragmentDownloadBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
 }
